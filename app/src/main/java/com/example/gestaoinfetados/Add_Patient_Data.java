fb0d1dacda4431c474f2ece63e309b6769f33321
DanielSiob <48354047+danielsiob@users.noreply.github.com>
2020-04-24 19:08:36 +0100

    Adição da atividade de dados do paciente alteração dos nomes dos id

---
+++ app/src/main/java/com/example/gestaoinfetados/Add_Patient_Data.java
@@ -0,0 +1,14 @@
+package com.example.gestaoinfetados;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_Patient_Data extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__patient__data);
+    }
+}
