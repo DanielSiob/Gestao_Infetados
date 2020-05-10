5ff18bbd5bde8e55e0ff5db97cbc42a0a54eee84
DanielSiob <48354047+danielsiob@users.noreply.github.com>
2020-05-10 20:52:12 +0100

    introdução de um calendário

--- app/src/main/java/com/example/gestaoinfetados/Add_Patient_Data.java
+++ app/src/main/java/com/example/gestaoinfetados/Add_Patient_Data.java
@@ -2,13 +2,51 @@ package com.example.gestaoinfetados;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class Add_Patient_Data extends AppCompatActivity {
+
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add__patient__data);
+
+        mDisplayDate = (TextView) findViewById(R.id.TVDate);
+
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int ano = cal.get(Calendar.YEAR);
+                int mes = cal.get(Calendar.MONTH);
+                int dia = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog( Add_Patient_Data.this,
+                        android.R.style.Widget_Holo_ActionBar_Solid,
+                        mDateSetListener,
+                        ano,mes,dia);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2){
+                Log.d(TAG, "onDateSet: " + i + "/" + i1 + "/" + i2);
+            }
+        };
     }
 }
