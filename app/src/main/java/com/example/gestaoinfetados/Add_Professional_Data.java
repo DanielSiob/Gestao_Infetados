e193e97a3a761106b4c693639216f95393491641
DanielSiob <48354047+danielsiob@users.noreply.github.com>
2020-06-21 16:00:01 +0100

    Introdução de novo Spinner, botões de Pick Date" e botões "Back"

--- app/src/main/java/com/example/gestaoinfetados/Add_Professional_Data.java
+++ app/src/main/java/com/example/gestaoinfetados/Add_Professional_Data.java
@@ -2,21 +2,57 @@ package com.example.gestaoinfetados;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 public class Add_Professional_Data extends AppCompatActivity  implements AdapterView.OnItemSelectedListener {
+
+    TextView mTVN;
+    Button mBtnN;
+
+    Calendar c;
+    DatePickerDialog dpd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add__professional__data);
 
+        mTVN = (TextView)findViewById(R.id.TextViewPickDateOfBirthProf);
+        mBtnN = (Button)findViewById(R.id.btnPickDateOfBirthProf);
 
+
+        //ESCOLHER DATA DE NASCIMENTO DOS PROFISSIONAIS
+        mBtnN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                c = Calendar.getInstance();
+                int dia = c.get(Calendar.DAY_OF_MONTH);
+                int mes = c.get(Calendar.MONTH);
+                int ano = c.get(Calendar.YEAR);
+
+                dpd = new DatePickerDialog(Add_Professional_Data.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        mTVN.setText(dayOfMonth + "/" + (month+1) + "/" + year);
+                    }
+                }, dia, mes, ano);
+                dpd.show();
+            }
+        });
+
+
+        //SPINNER DA SECÇÃO DOS TRABALHOS DOS PROFISSIONAIS
         Spinner spinner = findViewById(R.id.spinner1);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.seccao, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
--- app/src/main/java/com/example/gestaoinfetados/Edit_Patient_Info.java
+++ app/src/main/java/com/example/gestaoinfetados/Edit_Patient_Info.java
@@ -5,16 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
-public class Edit_Patient_Info extends AppCompatActivity {
+public class Edit_Patient_Info extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     TextView mTVEh;
     Button mBtnEh;
+    TextView mTVAh;
+    Button mBtnAh;
+    TextView mTVObi;
+    Button mBtnObi;
 
     Calendar c;
     DatePickerDialog dpd;
@@ -26,6 +34,14 @@ public class Edit_Patient_Info extends AppCompatActivity {
 
         mTVEh = (TextView)findViewById(R.id.TextViewPickDateEntHosp);
         mBtnEh = (Button)findViewById(R.id.btnPickDateEntHosp);
+
+        mTVAh = (TextView)findViewById(R.id.TVAltaHosp);
+        mBtnAh = (Button) findViewById(R.id.btnPickDateAltaHosp);
+
+        mTVObi = (TextView)findViewById(R.id.TextViewPickDateDataObi);
+        mBtnObi = (Button)findViewById(R.id.btnPickDateDataObi);
+
+        //ESCOLHER DATA PARA ENTRADA HOSPITALAR
 
         mBtnEh.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,5 +61,63 @@ public class Edit_Patient_Info extends AppCompatActivity {
                     dpd.show();
             }
         });
+
+        //ESCOLHER DATA PARA SAIDA HOSPITALAR
+        mBtnAh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                c = Calendar.getInstance();
+                int dia = c.get(Calendar.DAY_OF_MONTH);
+                int mes = c.get(Calendar.MONTH);
+                int ano = c.get(Calendar.YEAR);
+
+                dpd = new DatePickerDialog(Edit_Patient_Info.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        mTVAh.setText(dayOfMonth + "/" + (month+1) + "/" + year);
+                    }
+                }, dia, mes, ano);
+                    dpd.show();
+            }
+        });
+        //ESCOLHER DATA DE OBITO DO PACIENTE
+        mBtnObi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                c = Calendar.getInstance();
+                int dia = c.get(Calendar.DAY_OF_MONTH);
+                int mes = c.get(Calendar.MONTH);
+                int ano = c.get(Calendar.YEAR);
+
+                dpd = new DatePickerDialog(Edit_Patient_Info.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        mTVObi.setText(dayOfMonth + "/" + (month+1) + "/" + year);
+                    }
+                }, dia, mes, ano);
+                dpd.show();
+            }
+        });
+
+
+        //SPINNER DOS SINTOMAS
+
+        Spinner spinner = findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.sintomas, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
